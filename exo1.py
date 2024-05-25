d0191149ae5dba232a0e809248bb3dbb608a7f23
maghen2 <maghen2@gmail.com>
2024-05-25 04:11:58 +0200

    Exercice 1 : Calculer la circonference et la surface d'un cercle dont l'utilisateur renseigne le rayon

---
+++ exo1.py
@@ -0,0 +1,13 @@
+# Apprendre à coder avec Python - Université Libre de Bruxelles (ULB)
+#https://www.fun-mooc.fr/en/cours/apprendre-a-coder-avec-python/
+
+# Exercice 1 : Calculer la circonference et la surface d'un cercle dont l'utilisateur renseigne le rayon
+import math
+from math import pi
+rayon = float(input("Veuillez entrer la valeur du rayon du cercle ex: 5.2 ou 3 par exemple")) # invite l'utilisateur à renseigne la valeur du rayon
+
+circonference = rayon*2*math.pi*2
+surface = rayon**2*math.pi
+print("le rayon est de         : ",rayon)
+print("la circonférence est de : ",circonference)
+print("la surface est de       : ",surface)
