b0cec138f8ebd7204b85cdcde3fc562f66d6f478
maghen2 <maghen2@gmail.com>
2024-05-26 05:20:39 +0200

    commente

---
+++ 3-Instructions/Exercice-UpyLab-3.2.py
@@ -0,0 +1,12 @@
+# Exercice UpyLab 3.2 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+
+# Écrire un programme qui, si temperature (entier lu sur input correspondant à la température maximale prévue pour aujourd’hui) est strictement supérieur à 0, teste si temperature est inférieur ou égal à 10, auquel cas il imprime le texte :
+
+# Il va faire frais.
+
+# et qui, si temperature n’est pas supérieur à 0, imprime le texte :
+
+# Il va faire froid.
+
+# Dans les autres cas, le programme n’imprime rien.
