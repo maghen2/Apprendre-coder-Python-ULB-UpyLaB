dbfc78fd3622b703ad1598f6fa72c2177abc84d6
maghen2 <maghen2@gmail.com>
2024-05-26 04:46:22 +0200

    UpyLab 2.7

--- 2-Arithmétique-et-manipulation-des-textes/Exercice-UpyLab-2.7.py
+++ 2-Arithmétique-et-manipulation-des-textes/Exercice-UpyLab-2.7.py
@@ -6,6 +6,6 @@ import math
 
 # On rappelle que le volume d’une sphère de rayon r est donné par la formule : \frac{4}{3}\pi{r^3} V = 4/3πr³.
 
-r = float(input()
+r = float(input())
 volume = (4/3) * math.pi * (r ** 3)
 print(volume)
