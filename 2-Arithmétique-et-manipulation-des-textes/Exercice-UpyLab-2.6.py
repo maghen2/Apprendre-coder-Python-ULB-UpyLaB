3a9586cd46a5f90a9c54af9e7b84e66413157648
maghen2 <maghen2@gmail.com>
2024-05-26 04:36:49 +0200

    debug code

---
+++ 2-Arithmétique-et-manipulation-des-textes/Exercice-UpyLab-2.6.py
@@ -0,0 +1,14 @@
+# Exercice UpyLab 2.6 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+#  Le but de cet exercice est de vous familiariser avec l’impression de certains caractères particuliers comme les simples et double quotes (apostrophes), l’anti-slash  \ (appelée également barre oblique inversée), …
+
+# Écrire un programme affichant les quatre lignes suivantes :
+
+# Hello World
+# Aujourd'hui
+# C'est "Dommage !"
+# Hum \o/
+print("Hello World")
+print("Aujourd'hui")
+print('C\'est "Dommage !"')
+print("Hum \\o/")
