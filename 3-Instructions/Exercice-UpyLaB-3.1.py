a3fc8307e798c059547067c401853ea287362ca2
maghen2 <maghen2@gmail.com>
2024-05-26 04:47:57 +0200

    3. Instructions

---
+++ 3-Instructions/Exercice-UpyLaB-3.1.py
@@ -0,0 +1,3 @@
+# Exercice UpyLaB 3.1 - Parcours vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire un programme qui lit 3 nombres entiers, et qui, si au moins deux d’entre eux ont la même valeur, imprime cette valeur (le programme n’imprime rien dans le cas contraire).
