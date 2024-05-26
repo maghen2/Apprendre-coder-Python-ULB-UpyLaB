3609429f2d9658b5c740888d974fa2dc1867800b
maghen2 <maghen2@gmail.com>
2024-05-26 04:06:05 +0200

    Exercice UpyLab 2.3

--- 2-Arithmétique-et-manipulation-des-textes/Exercice-UpyLab-2.3.py
+++ 2-Arithmétique-et-manipulation-des-textes/Exercice-UpyLab-2.3.py
@@ -16,3 +16,8 @@
 
 # Écrire un programme qui lit des valeurs de type float pour a, b et c et qui affiche la valeur de d vérifiant l'égalité \frac{a}{b} = \frac{c}{d}.
 
+a = float(input())
+b = float(input())
+c = float(input())
+d = (b * c) / a
+print(d)
