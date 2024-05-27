19265f5c42a462ac54453ab1d159e18783c6220d
maghen2 <maghen2@gmail.com>
2024-05-27 06:24:10 +0200

    Exercice UpyLab 3.5

---
+++ 3-Instructions/Exercice-UpyLab-3.5.py
@@ -0,0 +1,13 @@
+Exercice UpyLab 3.5 - Parcours : vert bleu rouge
+Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+
+Écrire un programme qui lit en entrée deux nombres entiers strictement positifs, et qui vérifie qu’aucun des deux n’est un diviseur de l’autre.
+
+Si tel est bien le cas, le programme imprime True. Sinon, il imprime False.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 % num2 != 0 and num2 % num1 != 0:
+    print(True)
+else:
+    print(False)
