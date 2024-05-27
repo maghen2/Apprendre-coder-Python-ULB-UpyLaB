4bb3e1e265455873f07cf9f91fcc50b16e5c1d22
maghen2 <maghen2@gmail.com>
2024-05-27 06:15:18 +0200

    Exercice UpyLab 3.3

---
+++ 3-Instructions/Exercice-UpyLab-3.3.py
@@ -0,0 +1,13 @@
+# Exercice UpyLab 3.3 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire un programme qui lit trois entiers a, b et c en input. Ensuite :
+
+# si l’entier c est égal à 1, alors le programme affiche la valeur de a + b ;
+
+# si c vaut 2, alors le programme affiche la valeur de a - b ;
+
+# si c est égal à 3, alors l’output sera la valeur de a.b (produit de a par b) ;
+
+# enfin, si la valeur 4 est assignée à la variable c, alors le programme affiche la valeur de a^2 + a.b ;
+
+# et si c contient une autre valeur, le programme affiche le message Erreur.
