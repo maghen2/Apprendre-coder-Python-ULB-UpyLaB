09221b55d427ba9fa9248670f9e84c6ac003b445
maghen2 <maghen2@gmail.com>
2024-05-27 06:21:26 +0200

    Exercice UpyLab 3.4

---
+++ 3-Instructions/Exercice-UpyLab-3.4.py
@@ -0,0 +1,3 @@
+# Exercice UpyLab 3.4 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire un programme qui teste la parité d’un nombre entier lu sur input et imprime True si le nombre est pair, False dans le cas contraire.
