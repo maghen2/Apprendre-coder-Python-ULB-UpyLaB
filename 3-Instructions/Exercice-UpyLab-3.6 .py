81f2d8bb5ad20eecf757dc0882be205000f4e705
maghen2 <maghen2@gmail.com>
2024-05-27 06:31:37 +0200

    Exercice UpyLab 3.6

---
+++ 3-Instructions/Exercice-UpyLab-3.6 .py
@@ -0,0 +1,6 @@
+Exercice UpyLab 3.6 - Parcours : vert bleu rouge
+Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+
+Écrire un programme qui imprime la moyenne géométrique \sqrt{a.b} (la racine carrée du produit de a par b) de deux nombres positifs a et b de type float lus en entrée.
+
+Si au moins un de ces nombres est strictement négatif, le programme imprime le texte « Erreur ».
