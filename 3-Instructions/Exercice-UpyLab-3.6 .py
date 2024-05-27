c113f18ec8a98e875367d8b48ba91b8537e32b6e
maghen2 <maghen2@gmail.com>
2024-05-27 06:33:39 +0200

    feat: Add code to calculate geometric mean of two positive numbers

--- 3-Instructions/Exercice-UpyLab-3.6 .py
+++ 3-Instructions/Exercice-UpyLab-3.6 .py
@@ -1,6 +1,16 @@
-Exercice UpyLab 3.6 - Parcours : vert bleu rouge
-Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+import math
 
-Écrire un programme qui imprime la moyenne géométrique \sqrt{a.b} (la racine carrée du produit de a par b) de deux nombres positifs a et b de type float lus en entrée.
+# Exercice UpyLab 3.6 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
 
-Si au moins un de ces nombres est strictement négatif, le programme imprime le texte « Erreur ».
+# Écrire un programme qui imprime la moyenne géométrique \sqrt{a.b} (la racine carrée du produit de a par b) de deux nombres positifs a et b de type float lus en entrée.
+
+# Si au moins un de ces nombres est strictement négatif, le programme imprime le texte « Erreur ».
+a = float(input())
+b = float(input())
+
+if a < 0 or b < 0:
+    print("Erreur")
+else:
+    result = math.sqrt(a * b)
+    print("The geometric mean is:", result)
