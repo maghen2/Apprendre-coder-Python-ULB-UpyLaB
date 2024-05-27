1b3075b2cef65685afb030c751e07e233c1e1d08
maghen2 <maghen2@gmail.com>
2024-05-27 06:22:58 +0200

    feat: Add code to test and print the parity of an input number

--- 3-Instructions/Exercice-UpyLab-3.4.py
+++ 3-Instructions/Exercice-UpyLab-3.4.py
@@ -1,3 +1,8 @@
 # Exercice UpyLab 3.4 - Parcours : vert bleu rouge
 # Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
 # Écrire un programme qui teste la parité d’un nombre entier lu sur input et imprime True si le nombre est pair, False dans le cas contraire.
+number = int(input())
+if number % 2 == 0:
+    print(True)
+else:
+    print(False)
