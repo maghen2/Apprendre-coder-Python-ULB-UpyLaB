7cdbc58793832d02c282e054ddc5bab1f09bead5
maghen2 <maghen2@gmail.com>
2024-05-27 06:45:35 +0200

    Add code to print the color of each number in a list

--- 3-Instructions/Exercice-UpyLab-3.7.py
+++ 3-Instructions/Exercice-UpyLab-3.7.py
@@ -5,4 +5,9 @@
 # https://studio.fun-mooc.fr/asset-v1:ulb+44013+session03+type@asset+block/roulette.jpg
 # Roulette
 
-
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for numero in numeros:
+    if numero % 2 == 0:
+        print(f"{numero} est de couleur rouge")
+    else:
+        print(f"{numero} est de couleur bleue")
