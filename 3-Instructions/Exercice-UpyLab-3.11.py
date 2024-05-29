32b0fcfb40a56b3e38b75a3bd7f7706fb7423757
maghen2 <maghen2@gmail.com>
2024-05-29 03:40:27 +0200

    Exercice UpyLab 3.11

---
+++ 3-Instructions/Exercice-UpyLab-3.11.py
@@ -0,0 +1,3 @@
+# Exercice UpyLab 3.11 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire un programme qui lit sur input une valeur naturelle n et qui affiche à l’écran un carré de n caractères X (majuscule) de côté.
