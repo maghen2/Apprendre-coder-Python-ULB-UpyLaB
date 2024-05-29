4d7ba280be757a6d8f81d1f4408736c2b755c14e
maghen2 <maghen2@gmail.com>
2024-05-29 04:00:41 +0200

    feat: Add code to sum natural numbers
    The code changes add functionality to read a series of natural numbers from input and calculate their sum. The first number read determines the number of values to be summed. If the first number is non-negative, it represents the count of values to be read and summed. If the first number is -1, it indicates that the values will be read until the character "F" is encountered. This change improves the program by allowing the user to input a variable number of values to be summed.

--- 3-Instructions/Exercice-UpyLab-3.13.py
+++ 3-Instructions/Exercice-UpyLab-3.13.py
@@ -1,18 +1,25 @@
 # Exercice UpyLab 3.13 - Parcours : bleu rouge
 # Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
-# Cet exercice propose une variante de l’exercice précédent sur le carré de X.
+# Écrire un programme qui additionne des valeurs naturelles lues sur entrée et affiche le résultat.
+# La première donnée lue ne fait pas partie des valeurs à sommer. Elle détermine si la liste contient un nombre déterminé à l’avance de valeurs à lire ou non :
 
-# Écrire un programme qui lit en entrée une valeur naturelle n et qui affiche à l’écran un triangle supérieur droit formé de X (voir exemples plus bas).
+# si cette valeur est un nombre positif ou nul, elle donne le nombre de valeurs à lire et à sommer ;
 
-# Exemple 1
-# Avec la donnée lue suivante :
+# si elle est égale à -1, cela signifie qu’elle est suivie d’une liste de données à lire qui sera terminée par le caractère "F" signifiant que la liste est terminée.
+total = count = 0
+num_values = int(input()) # Read the number of values to sum
 
-# 6
-# le résultat à imprimer vaudra :
+if num_values >= 0:
+    for count in range(num_values):
+        value = int(input()) # Read a value
+        total += value
+        count += 1
 
-# XXXXXX
-#  XXXXX
-#   XXXX
-#    XXX
-#     XX
-#      X
+elif num_values == -1:
+    while True:
+        value = input() # Read a value (or "F" to finish)
+        if value == "F":
+            break
+        total += int(value)
+
+print(total)
