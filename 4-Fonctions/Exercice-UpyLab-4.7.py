90d9c14d91b71c60e3a9d1a989845a3e569c6857
maghen2 <maghen2@gmail.com>
2024-05-30 01:11:32 +0200

    Exercice UpyLab 4.7

---
+++ 4-Fonctions/Exercice-UpyLab-4.7.py
@@ -0,0 +1,4 @@
+# Exercice UpyLab 4.7 - Parcours : bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction rac_eq_2nd_deg(a, b, c) qui reçoit trois paramètres de type float correspondant aux trois coefficients de l’équation du second degré ax^2 + bx + c = 0, avec a différent de 0, et qui renvoie la ou les solutions s’il y en a, sous forme d’un tuple.
+
