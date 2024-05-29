f34f13245046cf4756d0aabd83ecd06694360b1f
maghen2 <maghen2@gmail.com>
2024-05-29 03:48:58 +0200

    feat: Add code to print a right triangle of size n
    The code changes add functionality to print a right triangle of size n, where n is a natural number input by the user. Each line of the triangle is made up of the character 'X'. This change improves the program by allowing the user to specify the size of the triangle to be printed.

--- 3-Instructions/Exercice-UpyLab-3.12.py
+++ 3-Instructions/Exercice-UpyLab-3.12.py
@@ -3,3 +3,20 @@
 # Cet exercice propose une variante de l’exercice précédent sur le carré de X.
 
 # Écrire un programme qui lit en entrée une valeur naturelle n et qui affiche à l’écran un triangle supérieur droit formé de X (voir exemples plus bas).
+# Exemple 1
+# Avec la donnée lue suivante :
+
+# 6
+# le résultat à imprimer vaudra :
+
+# XXXXXX
+#  XXXXX
+#   XXXX
+#    XXX
+#     XX
+#      X
+
+n = int(input()) # Read the size of the triangle (number of lines)
+
+for i in range(n):
+    print(" " * i + "X" * (n - i))
