0a28b61639372814371dab930974ff054a94952d
maghen2 <maghen2@gmail.com>
2024-05-30 01:10:16 +0200

    feat: Add default values to somme function
    The code changes in `Exercice-UpyLab-4.6.py` add default values to the `somme` function. The function now accepts two integer parameters `a` and `b`, with default values of 0 and 1 respectively. This enhancement improves the flexibility of the function by allowing it to be called with or without arguments, providing default values when necessary.

--- 4-Fonctions/Exercice-UpyLab-4.6.py
+++ 4-Fonctions/Exercice-UpyLab-4.6.py
@@ -2,3 +2,5 @@
 # Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
 # Dans cet exercice, nous allons mettre en pratique la notion de valeur par défaut des paramètres d’une fonction.
 # Écrire une fonction somme(a, b) qui retourne la somme de deux valeurs entières a et b. Par défaut, la valeur de a est 0 et la valeur de b est 1.
+def somme(a=0, b=1):
+    return a + b
