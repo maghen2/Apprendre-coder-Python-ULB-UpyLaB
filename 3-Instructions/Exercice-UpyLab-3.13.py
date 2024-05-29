dd039107ad56dce53348e23c405070ad435bd09b
maghen2 <maghen2@gmail.com>
2024-05-29 03:51:04 +0200

    Exercice UpyLab 3.13

---
+++ 3-Instructions/Exercice-UpyLab-3.13.py
@@ -0,0 +1,18 @@
+# Exercice UpyLab 3.13 - Parcours : bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Cet exercice propose une variante de l’exercice précédent sur le carré de X.
+
+# Écrire un programme qui lit en entrée une valeur naturelle n et qui affiche à l’écran un triangle supérieur droit formé de X (voir exemples plus bas).
+
+# Exemple 1
+# Avec la donnée lue suivante :
+
+# 6
+# le résultat à imprimer vaudra :
+
+# XXXXXX
+#  XXXXX
+#   XXXX
+#    XXX
+#     XX
+#      X
