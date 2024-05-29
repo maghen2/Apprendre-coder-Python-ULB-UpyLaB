9343c45f56d717ec542235abd150343088fb91ce
maghen2 <maghen2@gmail.com>
2024-05-30 00:20:52 +0200

    Exercice UpyLab 4.3

---
+++ 4-Fonctions/Exercice-UpyLab-4.3.py
@@ -0,0 +1,5 @@
+# Exercice UpyLab 4.3 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction booléenne premier(n) qui reçoit un nombre entier positif n et qui renvoie True si n est un nombre premier, et False sinon.
+# Ensuite, écrire un programme qui lit une valeur entière x et affiche, grâce à des appels à la fonction premier, tous les nombres premiers strictement inférieurs à x, chacun sur sa propre ligne.
+
