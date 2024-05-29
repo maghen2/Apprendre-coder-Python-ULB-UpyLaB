8d41ff1ad97f51f09d59757cf6d51858c74b2f0d
maghen2 <maghen2@gmail.com>
2024-05-30 00:35:42 +0200

    Exercice UpyLab 4.4

---
+++ 4-Fonctions/Exercice-UpyLab-4.4.py
@@ -0,0 +1,5 @@
+# Exercice UpyLab 4.4 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction alea_dice(s) qui génère trois nombres (pseudo) aléatoires à l’aide de la fonction randint du module random, représentant trois dés (à six faces avec les valeurs de 1 à 6), et qui renvoie la valeur booléenne True si les dés forment un 421, et la valeur booléenne False sinon.
+
+# Le paramètre s de la fonction est un nombre entier, qui sera passé en argument à la fonction random.seed au début du code de la fonction. Cela permettra de générer la même suite de nombres aléatoires à chaque appel de la fonction, et ainsi de pouvoir tester son fonctionnement.
