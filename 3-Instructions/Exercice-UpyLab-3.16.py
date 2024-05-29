cb8ac5e2bfc60a0d3c4e89d3dd7fcea9b0597d13
maghen2 <maghen2@gmail.com>
2024-05-29 05:57:23 +0200

    Exercice UpyLab 3.16

---
+++ 3-Instructions/Exercice-UpyLab-3.16.py
@@ -0,0 +1,4 @@
+# Exercice UpyLab 3.16 - Parcours : rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrivez un code qui lit un nombre entier strictement positif n et affiche sur n lignes une table de multiplication de taille  n x n, avec, pour i entre 1 et n,  les n premières valeurs multiples de i strictement positives sur la ième ligne.
+# Ainsi, les n premiers multiples de 1 strictement positifs (0 non compris) sont affichés sur la première ligne, les n premiers multiples de 2 sur la deuxième, et cætera.
