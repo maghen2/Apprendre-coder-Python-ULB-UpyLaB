04fbe219e80b60a7f2169019b8733dd3e4c68be8
maghen2 <maghen2@gmail.com>
2024-05-29 03:45:03 +0200

    Exercice UpyLab 3.12

--- 3-Instructions/Exercice-UpyLab-3.11.py
+++ 3-Instructions/Exercice-UpyLab-3.11.py
@@ -4,3 +4,4 @@
 n = int(input())
 for i in range(n):
     print("X" * n)
+    
---
+++ 3-Instructions/Exercice-UpyLab-3.12.py
@@ -0,0 +1,5 @@
+# Exercice UpyLab 3.12 - Parcours : bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Cet exercice propose une variante de l’exercice précédent sur le carré de X.
+
+# Écrire un programme qui lit en entrée une valeur naturelle n et qui affiche à l’écran un triangle supérieur droit formé de X (voir exemples plus bas).
