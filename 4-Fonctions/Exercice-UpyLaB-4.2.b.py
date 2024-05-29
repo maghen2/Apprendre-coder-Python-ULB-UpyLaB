9036d2acc301e641963f8783397f463884d49181
maghen2 <maghen2@gmail.com>
2024-05-30 00:14:11 +0200

    Exercice UpyLaB 4.2.b

--- 4-Fonctions/Exercice-UpyLaB-4.2.b.py
+++ 4-Fonctions/Exercice-UpyLaB-4.2.b.py
@@ -1,4 +1,4 @@
-# Exercice UpyLaB 4.2 - Parcours vert bleu rouge
+# Exercice UpyLaB 4.2.b - Parcours vert bleu rouge
 # Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
 # Attention : cet exercice est composé de l'exercice UpyLaB 4.2.a suivi en dessous de l'exercice UpyLaB 4.2.b.
 
@@ -12,22 +12,6 @@
 # Heureusement pour nous, une journée U357 s'écoule sur 24 heures comme sur notre Terre, et pour simplifier, nous ne prendrons pas en compte les minutes (on ne donne que les heures avec des valeurs entières entre 0 et 23). 
 
 # Nous vous demandons d'aider le Petit Prince à déterminer les périodes de jour et de nuit.
-
-# UPYLAB 4.2A
-
-# Pour cela, vous allez dans un premier temps écrire une fonction soleil_leve qui, pour un soleil particulier, reçoit trois valeurs entières en argument pour la planète :
-#      - l'heure de lever du soleil (entre 0 et 23)
-#      - l'heure du coucher du soleil (entre 0 et 23)
-#      - l'heure actuelle (entre 0 et 23)
-
-# et qui renvoie une valeur booléenne vraie si le soleil est levé sur la planète à l'heure donnée en troisième argument et fausse, s'il est couché.
-
-# On supposera que chacun des soleils ne se lève et ne se couche au plus qu'une seule fois par jour.
-# Il est toutefois possible que le lever ait lieu après l'heure du coucher, ce qui signifie dans ce cas que le soleil est levé au début de la journée, puis qu'il se couche, puis qu'il se lève à nouveau plus tard dans la journée.
-# Enfin, si l'heure du lever est la même que l'heure du coucher :
-#      - soit toutes deux valent 12, cela signifie que le soleil ne se lève pas de la journée,
-#      - soit toutes les deux valent 0, cela signifie que le soleil ne se couche pas de la journée.
-
 
 def soleil_leve(heure_lever, heure_coucher, heure_actuelle):
     # Check if sunrise and sunset times are the same
@@ -49,7 +33,26 @@ def soleil_leve(heure_lever, heure_coucher, heure_actuelle):
             return True
     return False
 
-print(soleil_leve(6, 18, 10)) # doit retourner True
-print(soleil_leve(15, 8, 12))  # doit retourner False
-print(soleil_leve(12, 12, 10)) # doit retourner False
-print(soleil_leve(0, 0, 22)) # doit retourner True
+# UPYLAB 4.2B
+
+# Il vous faut maintenant écrire un programme qui lit en entrée :
+
+ 
+# . l'heure de lever du soleil E1515
+# . l'heure du coucher du soleil E1515
+# . l'heure de lever du soleil E666
+# . l'heure du coucher du soleil E666
+
+# et qui utilise la fonction soleil_leve pour afficher ligne par ligne chacune des heures de la journée, depuis 0 jusqu'à 23, suivies d'une espace et d'une astérisque s'il fait nuit à cette heure.
+
+# Attention, il ne fera nuit que si E1515 et E666 sont tous deux couchés.
+heure_lever_E1515 = int(input())
+heure_coucher_E1515 = int(input())
+heure_lever_E666 = int(input())
+heure_coucher_E666 = int(input())
+
+for heure in range(24):
+    if soleil_leve(heure_lever_E1515, heure_coucher_E1515, heure) or soleil_leve(heure_lever_E666, heure_coucher_E666, heure):
+        print(heure, end=" \n")
+    else:
+        print(heure, end=" * \n")
