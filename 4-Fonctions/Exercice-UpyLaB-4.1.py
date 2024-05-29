89c6d63b7b0d6dd6e18feb01f08e071e33a1ca44
maghen2 <maghen2@gmail.com>
2024-05-29 06:30:25 +0200

    Exercice UpyLaB 4.1

---
+++ 4-Fonctions/Exercice-UpyLaB-4.1.py
@@ -0,0 +1,6 @@
+# Exercice UpyLaB 4.1 - Parcours vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+ 
+# Écrire une fonction deux_egaux(a, b, c) qui reçoit trois nombres en paramètre et qui renvoie la valeur booléenne True si au moins deux de ces nombres ont la même valeur, et la valeur booléenne False sinon.
+
+# Ensuite, écrire un programme qui lit trois données de type int, x, y et z, et affiche le résultat de l’exécution de deux_egaux(x, y, z).
