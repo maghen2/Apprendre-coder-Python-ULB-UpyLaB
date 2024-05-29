c03154345e6dbe8fb209c5f4b7a05d60b85a818a
maghen2 <maghen2@gmail.com>
2024-05-30 01:09:04 +0200

    Exercice UpyLab 4.6

---
+++ 4-Fonctions/Exercice-UpyLab-4.6.py
@@ -0,0 +1,4 @@
+# Exercice UpyLab 4.6 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Dans cet exercice, nous allons mettre en pratique la notion de valeur par défaut des paramètres d’une fonction.
+# Écrire une fonction somme(a, b) qui retourne la somme de deux valeurs entières a et b. Par défaut, la valeur de a est 0 et la valeur de b est 1.
