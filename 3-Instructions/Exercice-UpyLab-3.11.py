82ab01be23fab6c73ea57adbedae3ce8bedf83c2
maghen2 <maghen2@gmail.com>
2024-05-29 03:42:16 +0200

    feat: Add code to print a square of size n
    The code changes add functionality to print a square of size n, where n is a natural number input by the user. Each side of the square is made up of the character 'X'. This change improves the program by allowing the user to specify the size of the square to be printed.

--- 3-Instructions/Exercice-UpyLab-3.11.py
+++ 3-Instructions/Exercice-UpyLab-3.11.py
@@ -1,3 +1,6 @@
 # Exercice UpyLab 3.11 - Parcours : vert bleu rouge
 # Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
 # Écrire un programme qui lit sur input une valeur naturelle n et qui affiche à l’écran un carré de n caractères X (majuscule) de côté.
+n = int(input())
+for i in range(n):
+    print("X" * n)
