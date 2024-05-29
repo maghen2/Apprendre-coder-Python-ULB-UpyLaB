24fef09151fcd83382cbb8958c13119b50928b4f
maghen2 <maghen2@gmail.com>
2024-05-29 06:34:10 +0200

    Refactor code for checking if at least two numbers are equal
    The code changes in `Exercice-UpyLaB-4.1.py` refactor the existing code for checking if at least two numbers have the same value. The function `deux_egaux(a, b, c)` has been modified to simplify the logic by using a single if statement with multiple conditions. This refactoring improves the readability and maintainability of the code.

--- 4-Fonctions/Exercice-UpyLaB-4.1.py
+++ 4-Fonctions/Exercice-UpyLaB-4.1.py
@@ -4,3 +4,15 @@
 # Écrire une fonction deux_egaux(a, b, c) qui reçoit trois nombres en paramètre et qui renvoie la valeur booléenne True si au moins deux de ces nombres ont la même valeur, et la valeur booléenne False sinon.
 
 # Ensuite, écrire un programme qui lit trois données de type int, x, y et z, et affiche le résultat de l’exécution de deux_egaux(x, y, z).
+def deux_egaux(a, b, c):
+    if a == b or a == c or b == c:
+        return True
+    else:
+        return False
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = deux_egaux(a, b, c)
+print(result)
