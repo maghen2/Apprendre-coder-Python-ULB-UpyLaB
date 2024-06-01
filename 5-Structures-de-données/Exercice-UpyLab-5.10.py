1c3e369da4bd37dec4ff63de5b66a84200cdeb95
maghen2 <maghen2@gmail.com>
2024-06-01 13:16:39 +0200

    anagrammes(v, w)

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.10.py
@@ -0,0 +1,6 @@
+# Exercice UpyLab 5.10 - Parcours : vert bleu rouge
+ 
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction dupliques qui reçoit une séquence en paramètre.
+
+# La fonction doit renvoyer la valeur booléenne True si la séquence passée en paramètre contient des éléments dupliqués, et la valeur booléenne False sinon.
