c3b2030cdb06b7ff912c958c8afdfd46a2d55f52
maghen2 <maghen2@gmail.com>
2024-06-01 04:31:06 +0200

    Refactor est_adn function for improved readability and efficiency

--- 5-Structures-de-données/Exercice-UpyLab-5.2.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.2.py
@@ -4,3 +4,9 @@
 # On représente un brin d’ADN par une chaîne de caractères dont les caractères sont parmi les quatre suivants : 'A' (Adénine), 'C' (Cytosine), 'G' (Guanine) et 'T' (Thymine).
 
 # Écrire une fonction est_adn qui reçoit une chaîne de caractères en paramètre et qui retourne True si cette chaîne de caractères n'est pas vide et peut représenter un brin d’ADN, False sinon.
+def est_adn(chaine):
+    if chaine and caractere in 'ACGT' for caractere in chaine:
+        return True
+    else:
+        return False
+    
