81436926d8cf244864da83bf854233388de35020
maghen2 <maghen2@gmail.com>
2024-06-01 21:54:20 +0200

    Write an acrostic function which receives the name of a file as a parameter and which returns the character string formed by the first letters of each line of the file.

--- 5-Structures-de-données/Exercice-UpyLab-5.19.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.19.py
@@ -3,3 +3,11 @@
 # D’après Wikipedia, un acrostiche est un poème, une strophe ou une série de strophes fondé sur une forme poétique consistant en ce que, lues verticalement de haut en bas, la première lettre ou, parfois, les premiers mots d’une suite de vers composent un mot ou une expression en lien avec le poème.
 
 # Écrire une fonction acrostiche qui reçoit en paramètre le nom d’un fichier et qui retourne la chaîne de caractères formée par les premières lettres de chaque ligne du fichier.
+def acrostiche(file_name):
+    acrostic = ""
+    with open(file_name, 'r') as file: #on ouvre le fichier
+        for line in file:
+            acrostic += line[0] #on recupère et cumule le premier caractère de chaque ligne
+    return acrostic
+
+print(acrostiche('Apollinaire.txt'))
