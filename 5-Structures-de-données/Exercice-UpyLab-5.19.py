b0e86ca3346cee8201fe61081e323745e4ad738e
maghen2 <maghen2@gmail.com>
2024-06-01 21:40:00 +0200

    Exercice UpyLab 5.19

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.19.py
@@ -0,0 +1,5 @@
+# Exercice UpyLab 5.19 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# D’après Wikipedia, un acrostiche est un poème, une strophe ou une série de strophes fondé sur une forme poétique consistant en ce que, lues verticalement de haut en bas, la première lettre ou, parfois, les premiers mots d’une suite de vers composent un mot ou une expression en lien avec le poème.
+
+# Écrire une fonction acrostiche qui reçoit en paramètre le nom d’un fichier et qui retourne la chaîne de caractères formée par les premières lettres de chaque ligne du fichier.
