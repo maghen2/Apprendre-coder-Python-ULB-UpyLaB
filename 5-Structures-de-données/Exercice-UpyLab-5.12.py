65c6adb12f20e93116b094035786a4b22c0348f9
maghen2 <maghen2@gmail.com>
2024-06-01 13:38:27 +0200

    Exercice UpyLab 5.12

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.12.py
@@ -0,0 +1,8 @@
+# Exercice UpyLab 5.12 - Parcours : bleu rouge
+ 
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction my_insert qui reçoit comme premier paramètre une liste d’entiers relatifs triée par ordre croissant et comme deuxième paramètre un entier relatif n, et qui renvoie une liste correspondant à la liste reçue, mais dans laquelle le nombre n a été inséré à la bonne place.
+
+# La liste donnée en paramètre ne doit pas être modifiée par votre fonction.
+
+# Vous pouvez supposer que le premier paramètre sera bien une liste triée d’entiers, mais si le deuxième paramètre n’est pas du bon type, la fonction retourne None.
