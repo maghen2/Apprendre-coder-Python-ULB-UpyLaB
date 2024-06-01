fbe3e0a591ed7ee5782d19a4e8a50a9d2144647f
maghen2 <maghen2@gmail.com>
2024-06-01 13:36:43 +0200

    Write an intersection(v, w) function that calculates the intersection between two character strings v and w.

--- 5-Structures-de-données/Exercice-UpyLab-5.11.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.11.py
@@ -8,3 +8,13 @@
 # Si les deux chaînes n’ont aucun caractère en commun, la fonction retourne la chaîne vide, ''.
 
 # Si plusieurs solutions sont possibles, la fonction retournera la sous-chaîne d’indice minimal dans v. Par exemple, intersection('bbaacc', 'aabb') renvoie 'bb'.
+def intersection(v, w):
+    common_part = ''
+    for i in range(len(v)):
+        for j in range(i+1, len(v)+1):
+            if v[i:j] in w and len(v[i:j]) > len(common_part):
+                common_part = v[i:j]
+    return common_part
+
+
+print(intersection('programme', 'grammaire')) 
