9ceccfdf4639eab375f2cc348d246c24c4c12052
maghen2 <maghen2@gmail.com>
2024-06-01 11:53:07 +0200

    Exercice UpyLab 5.6

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.6.py
@@ -0,0 +1,7 @@
+# Exercice UpyLab 5.6 - Parcours : vert bleu rouge
+ 
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction transcription_arn(brin_codant) qui reçoit une chaîne de caractères en paramètre, correspondant à un brin codant d'ADN, et qui retourne la chaîne de caractère représentant le brin d' ARN correspondant.
+
+# Nous rappelons qu'un brin d'ADN peut être modélisé par une chaîne de caractères, dont les caractères sont pris parmi les quatre suivants  : 'A'(Adénine), 'C' (Cytosine),'G' (Guanine) et 'T' (Thymine).
+# La transcription en ARN se traduit par le remplacement des nucléotides de Thymine par des nucléotides d'Uracile, que l'on représentera par le caractère 'U'.
