f3d19d21bd7f4192644ad9efaa4b846ca6368962
maghen2 <maghen2@gmail.com>
2024-06-01 18:30:12 +0200

    Exercice UpyLab 5.14

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.14.py
@@ -0,0 +1,9 @@
+# Exercice UpyLab 5.14 - Parcours : vert bleu rouge
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier, Jacky Trinh
+# Nous pouvons définir la distance entre deux mots de même longueur (c’est-à-dire ayant le même nombre de lettres) mot_1 et mot_2 comme le nombre minimum de fois où il faut modifier une lettre de mot_1 pour obtenir mot_2, sans changer leur place (distance de Hamming).
+
+# Par exemple, les mots « lire » et « bise » sont à une distance de 2, puisqu’il faut changer le “l” et le “r” du mot « lire » pour obtenir « bise ».
+
+# Écrire une fonction distance_mots(mot_1, mot_2) qui retourne la distance entre deux mots.
+
+# Vous pouvez supposer que les deux mots sont de même longueur, et sont écrits sans accents.
