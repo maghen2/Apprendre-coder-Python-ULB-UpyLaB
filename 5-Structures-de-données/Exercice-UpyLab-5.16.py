37b0e7292748a5ae1903f402890c99516d205d91
maghen2 <maghen2@gmail.com>
2024-06-01 20:12:48 +0200

    Exercice UpyLab 5.16

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.16.py
@@ -0,0 +1,8 @@
+# Exercice UpyLab 5.16 - Parcours : vert bleu rouge
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Cet exercice peut aussi se résoudre sans utiliser la technique de compréhension de liste. Mais les solutions sont beaucoup plus courtes avec cette technique ! Nous vous demandons donc si possible de les réaliser avec du code utilisant des compréhensions de liste.
+
+# Écrire une fonction my_pow qui prend comme paramètres un nombre entier m et un nombre flottant b, et qui renvoie une liste contenant les m premières puissances de b, c’est-à-dire une liste contenant les nombres allant de b^0 à b^{m - 1}.
+
+# Si le type des paramètres n’est pas celui attendu, la fonction retournera la valeur None.
+
