8b4da1c9ff277b3bfae715702244f1cd73dcb01d
maghen2 <maghen2@gmail.com>
2024-06-01 11:32:51 +0200

    distance_points function to calculate distance between 2 points

--- 5-Structures-de-données/Exercice-UpyLab-5.4.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.4.py
@@ -7,3 +7,9 @@
 # dist = \sqrt{(x_1 - x_2)^2 + (y_1 - y_2)^2}
 
 # où, si a désigne un nombre positif, \sqrt{a} désigne la racine carrée de a et correspond à a^{\frac{1}{2}}.
+def distance_points(a, b):
+    x = a[0] - b[0]
+    y = a[1] - b[1]
+    return (x**2 + y**2)**0.5
+
+print(distance_points((-1.0, 0.5), (2.0, 1.0)))
