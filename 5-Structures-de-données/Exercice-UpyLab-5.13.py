4a06211414c1433b695b77086119355255d315c3
maghen2 <maghen2@gmail.com>
2024-06-01 18:25:01 +0200

    Write a function my_insert which receives as first parameter a list of relative integers sorted in ascending order and as second parameter a relative integer n. The exercise is the same as the previous one, but here, if the parameters have the expected type, the function modifies the list in place and returns nothing. If the parameters are invalid, an error occurs at runtime.

--- 5-Structures-de-données/Exercice-UpyLab-5.13.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.13.py
@@ -1,18 +1,16 @@
-Exercice UpyLab 5.13 - Parcours : rouge
+# Exercice UpyLab 5.13 - Parcours : rouge
  
-Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
-L’exercice est le même que le précédent, mais ici, si les paramètres ont le type attendu, la fonction modifie la liste en place et ne retourne rien. Si les paramètres ne sont pas valides, une erreur se produit à l’exécution.
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# L’exercice est le même que le précédent, mais ici, si les paramètres ont le type attendu, la fonction modifie la liste en place et ne retourne rien. Si les paramètres ne sont pas valides, une erreur se produit à l’exécution.
 
 def my_insert(lst, n):
-    if not isinstance(n, int):
-        return None
-    
-    new_lst = lst.copy()
-    new_lst.append(n)
-    new_lst.sort()
-    
-    return new_lst
+    assert isinstance(n, int) #, "AssertionError"
+    lst.append(n)
+    lst.sort()
 
-print(my_insert([1, 3, 5], 4))
-print(my_insert([2, 3, 5], 1))
-print(my_insert([2, 3, 5], 0.5))
+lst = [1, 3, 5]
+my_insert(lst, 4)
+print(lst)
+
+my_insert(lst, 4.5)
+print(lst)
