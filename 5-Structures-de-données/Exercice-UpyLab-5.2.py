75f78b9bc611a1b90bcc6369fc5f9cfdae819cc3
maghen2 <maghen2@gmail.com>
2024-06-01 04:33:45 +0200

    correction of some minors bugs

--- 5-Structures-de-données/Exercice-UpyLab-5.2.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.2.py
@@ -5,8 +5,10 @@
 
 # Écrire une fonction est_adn qui reçoit une chaîne de caractères en paramètre et qui retourne True si cette chaîne de caractères n'est pas vide et peut représenter un brin d’ADN, False sinon.
 def est_adn(chaine):
-    if chaine and caractere in 'ACGT' for caractere in chaine:
+    if chaine and all(caractere in 'ACGT' for caractere in chaine):
         return True
     else:
         return False
     
+print(est_adn('ACGT'))
+print(est_adn("ARTGGT"))
