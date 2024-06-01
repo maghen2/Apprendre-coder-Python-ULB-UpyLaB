c2b9d83a578c1138c6b70ae6f86f19c159b54d7a
maghen2 <maghen2@gmail.com>
2024-06-01 12:02:26 +0200

    Add transcription_arn function for DNA to RNA transcription

--- 5-Structures-de-données/Exercice-UpyLab-5.6.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.6.py
@@ -5,3 +5,21 @@
 
 # Nous rappelons qu'un brin d'ADN peut être modélisé par une chaîne de caractères, dont les caractères sont pris parmi les quatre suivants  : 'A'(Adénine), 'C' (Cytosine),'G' (Guanine) et 'T' (Thymine).
 # La transcription en ARN se traduit par le remplacement des nucléotides de Thymine par des nucléotides d'Uracile, que l'on représentera par le caractère 'U'.
+# Exemple
+# L’appel suivant de la fonction :
+
+# transcription_arn('AGTCTTACCGATCCAT')
+# doit retourner :
+
+# 'AGUCUUACCGAUCCAU'
+def transcription_arn(brin_codant):
+    brin_arn = ""
+    for nucleotide in brin_codant:
+        if nucleotide == 'T':
+            brin_arn += 'U'
+        else:
+            brin_arn += nucleotide
+    return brin_arn
+
+
+print(transcription_arn('AGTCTTACCGATCCAT'))
