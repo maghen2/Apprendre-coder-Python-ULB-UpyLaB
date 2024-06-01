bc903d581de73efa41431e51825e7abd74f09e5c
maghen2 <maghen2@gmail.com>
2024-06-01 13:24:16 +0200

    function to track suplicate on a list

--- 5-Structures-de-données/Exercice-UpyLab-5.10.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.10.py
@@ -4,3 +4,5 @@
 # Écrire une fonction dupliques qui reçoit une séquence en paramètre.
 
 # La fonction doit renvoyer la valeur booléenne True si la séquence passée en paramètre contient des éléments dupliqués, et la valeur booléenne False sinon.
+def dupliques(sequence):
+    return len(sequence) != len(set(sequence)) # on compare la longueur de la séquence avec la longueur de la séquence transformée en ensemble car les ensembles n'admettent pas de doublon
