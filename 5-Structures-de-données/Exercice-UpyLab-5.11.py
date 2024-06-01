dd5013321b863f4155aa20d02e2f66dc77f2df17
maghen2 <maghen2@gmail.com>
2024-06-01 13:26:06 +0200

    Add dupliques function to check for duplicate elements in a sequence

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.11.py
@@ -0,0 +1,10 @@
+# Exercice UpyLab 5.11 - Parcours : bleu rouge
+ 
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction intersection(v, w) qui calcule l’intersection entre deux chaînes de caractères v et w.
+
+# On définit l’intersection de deux mots comme étant la plus grande partie commune à ces deux mots. Par exemple, l’intersection de « programme » et « grammaire » est « gramm ».
+
+# Si les deux chaînes n’ont aucun caractère en commun, la fonction retourne la chaîne vide, ''.
+
+# Si plusieurs solutions sont possibles, la fonction retournera la sous-chaîne d’indice minimal dans v. Par exemple, intersection('bbaacc', 'aabb') renvoie 'bb'.
