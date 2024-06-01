fdd0a3e65ff7f31903477de2669e8bf20b8ef2bb
maghen2 <maghen2@gmail.com>
2024-06-01 18:06:33 +0200

    Write a function my_insert which receives as first parameter a list of relative integers sorted in ascending order and as second parameter a relative integer n, and which returns a list corresponding to the list received, but in which the number n has been inserted at the correct place place.

--- 5-Structures-de-données/Exercice-UpyLab-5.12.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.12.py
@@ -6,3 +6,15 @@
 # La liste donnée en paramètre ne doit pas être modifiée par votre fonction.
 
 # Vous pouvez supposer que le premier paramètre sera bien une liste triée d’entiers, mais si le deuxième paramètre n’est pas du bon type, la fonction retourne None.
+def my_insert(lst, n):
+    if not isinstance(n, int):
+        return None
+    
+    new_lst = lst.copy()
+    new_lst.append(n)
+    new_lst.sort()
+    
+    return new_lst
+print(my_insert([1, 3, 5], 4))
+print(my_insert([2, 3, 5], 1))
+print(my_insert([2, 3, 5], 0.5))
