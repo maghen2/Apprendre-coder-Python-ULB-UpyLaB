010ec55eef882b3429c00b5c7424428ae7184247
maghen2 <maghen2@gmail.com>
2024-06-01 18:36:41 +0200

    Exercice UpyLab 5.15

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.15.py
@@ -0,0 +1,5 @@
+# Exercice UpyLab 5.15 - Parcours : rouge
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier, Jacky Trinh
+# Afin de l’aider, nous vous demandons d’écrire une fonction correcteur(mot, liste_mots) où mot est le mot que Joao écrit et liste_mots est une liste qui contient les mots (ayant la bonne orthographe) que Joao est susceptible d’utiliser.
+
+# Cette fonction doit retourner le mot dont l’orthographe a été corrigée.
