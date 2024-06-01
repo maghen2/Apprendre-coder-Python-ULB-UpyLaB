97778fd1b1e6a8f9c87454d5c3bdd44165882d03
maghen2 <maghen2@gmail.com>
2024-06-01 20:46:54 +0200

    Schreiben Sie eine Funktion my_pow, die als Parameter eine Ganzzahl m und eine Gleitzahl b akzeptiert und eine Liste mit den ersten m Potenzen von b zurückgibt, also eine Liste mit den Zahlen von b^0 bis b ^{m - 1}.

--- 5-Structures-de-données/Exercice-UpyLab-5.16.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.16.py
@@ -6,3 +6,11 @@
 
 # Si le type des paramètres n’est pas celui attendu, la fonction retournera la valeur None.
 
+def my_pow(m, b):
+    if not isinstance(m, int) or not isinstance(b, float):
+        return None
+    return [b**i for i in range(m)]
+
+print(my_pow(3, 5.0))
+print(my_pow(3.0, 5.0))
+print(my_pow('a', 'b'))
