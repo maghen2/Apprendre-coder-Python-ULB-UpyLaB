2c33b9b34d6018abcde28a9f4fc1e192d37558fa
maghen2 <maghen2@gmail.com>
2024-06-01 11:26:21 +0200

    Exercice UpyLab 5.4

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.4.py
@@ -0,0 +1,9 @@
+# Exercice UpyLab 5.4 - Parcours : vert bleu rouge
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+ 
+# Écrire une fonction distance_points() qui reçoit en paramètres deux tuples de deux composantes représentant les coordonnées de deux points et qui retourne la distance euclidienne séparant ces deux points.
+# Pour rappel, la distance euclidienne entre les points (x_1, y_1) et (x_2, y_2) se calcule grâce à la formule :
+
+# dist = \sqrt{(x_1 - x_2)^2 + (y_1 - y_2)^2}
+
+# où, si a désigne un nombre positif, \sqrt{a} désigne la racine carrée de a et correspond à a^{\frac{1}{2}}.
