4d37130d64dfe5840b89621802254a8dd90a4f37
maghen2 <maghen2@gmail.com>
2024-06-01 11:42:44 +0200

    Exercice UpyLab 5.5

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.5.py
@@ -0,0 +1,5 @@
+# Exercice UpyLab 5.5 - Parcours : bleu rouge
+# Auteurs Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+ 
+# Écrire une fonction longueur(*points) qui reçoit en paramètres un nombre arbitraire de points (tuples de deux composantes), et retourne la longueur de la ligne brisée correspondante.
+# Cette longueur se calcule en additionnant les longueurs des segments formés par deux points consécutifs.
