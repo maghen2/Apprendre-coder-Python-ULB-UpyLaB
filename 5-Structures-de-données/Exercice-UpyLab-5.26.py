fb78b9e137e05fd35f2700c410f6909601c966d3
maghen2 <maghen2@gmail.com>
2024-06-06 08:52:48 +0200

    Exercice UpyLab 5.26

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.26.py
@@ -0,0 +1,28 @@
+# Exercice UpyLab 5.26 - Parcours : bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Une matrice M = \{m_{ij}\} de taille {n}\times{n} est dite antisymétrique lorsque, pour toute paire d’indices i, j, on a m_{ij} = - m_{ji}.
+
+# Écrire une fonction booléenne antisymetrique(M) qui teste si la matrice M reçue est antisymétrique.
+
+# Exemple 1
+# L’appel suivant de la fonction :
+
+# antisymetrique([[0, 1, 1], [-1, 0, 1], [-1, -1, 0]])
+# doit retourner :
+
+# True
+# Exemple 2
+# L’appel suivant de la fonction :
+
+# antisymetrique([[0, 1], [1, 0]])
+# doit retourner :
+
+# False
+# Exemple 3
+# L’appel suivant de la fonction :
+
+# antisymetrique([[1, -2], [2, 1]])
+# doit retourner :
+
+# False
+
