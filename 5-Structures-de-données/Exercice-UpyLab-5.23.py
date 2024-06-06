4a0b8cf36629ec20d13b623c24b65e5b04464c5b
maghen2 <maghen2@gmail.com>
2024-06-06 06:10:24 +0200

    def init_mat(m, n):     return [[0] * n for _ in range(m)]

--- 5-Structures-de-données/Exercice-UpyLab-5.23.py
+++ 5-Structures-de-données/Exercice-UpyLab-5.23.py
@@ -16,23 +16,6 @@
 # doit retourner :
 
 # []
-# Consignes
-# Dans cet exercice, il vous est demandé d’écrire seulement la fonction init_mat.
 
-# Le code que vous soumettez à UpyLaB doit donc comporter uniquement la définition de cette fonction, et ne fait en particulier aucun appel à input ou à print.
-
-# AIDE EN CAS DE BESOIN
-# Conseils
-# Il n’est demandé ici que de définir la fonction. Mais pour tester son fonctionnement dans votre IDE, pensez à ajouter du code qui l’appelle et teste son résultat, sur plusieurs valeurs, comme print(init_mat(2, 2))par exemple.
-
-# Si votre fonction semble retourner le bon résultat, mais n’est pourtant pas acceptée par UpyLaB, nous vous proposons de tester le code suivant dans votre IDE (après la définition de la fonction) :
-
-# m = init_mat(3, 2)
-# m[0][0] = 1 #modifie le premier élément de la matrice
-# print(m)
-# Que se passe-t-il ?
-
-# N’hésitez pas à revoir la matière dispensée dans la section 5.9.1 dans le MOOC « Apprendre à coder avec Python » concernant l’initialisation de tableaux, ni à utiliser Python Tutor et observer en particulier les diagrammes d’état.
-
-# Si rien ne marche : consultez la FAQ sur UpyLaB 5.23.
-
+def init_mat(m, n):
+    return [[0] * n for _ in range(m)]
