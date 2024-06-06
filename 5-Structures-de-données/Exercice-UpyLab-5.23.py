84c5721270108d7b11ea732b8cd0d2becb4a599a
maghen2 <maghen2@gmail.com>
2024-06-06 06:08:00 +0200

    Exercice UpyLab 5.23

---
+++ 5-Structures-de-données/Exercice-UpyLab-5.23.py
@@ -0,0 +1,38 @@
+# Exercice UpyLab 5.23 - Parcours : vert bleu rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction init_mat(m, n) qui construit et renvoie une matrice d’entiers initialisée à la matrice nulle et de dimension m x n.
+
+# Exemple 1
+# L’appel suivant de la fonction :
+
+# init_mat(2, 3)
+# doit retourner :
+
+# [[0, 0, 0], [0, 0, 0]]
+# Exemple 2
+# L’appel suivant de la fonction :
+
+# init_mat(0, 0)
+# doit retourner :
+
+# []
+# Consignes
+# Dans cet exercice, il vous est demandé d’écrire seulement la fonction init_mat.
+
+# Le code que vous soumettez à UpyLaB doit donc comporter uniquement la définition de cette fonction, et ne fait en particulier aucun appel à input ou à print.
+
+# AIDE EN CAS DE BESOIN
+# Conseils
+# Il n’est demandé ici que de définir la fonction. Mais pour tester son fonctionnement dans votre IDE, pensez à ajouter du code qui l’appelle et teste son résultat, sur plusieurs valeurs, comme print(init_mat(2, 2))par exemple.
+
+# Si votre fonction semble retourner le bon résultat, mais n’est pourtant pas acceptée par UpyLaB, nous vous proposons de tester le code suivant dans votre IDE (après la définition de la fonction) :
+
+# m = init_mat(3, 2)
+# m[0][0] = 1 #modifie le premier élément de la matrice
+# print(m)
+# Que se passe-t-il ?
+
+# N’hésitez pas à revoir la matière dispensée dans la section 5.9.1 dans le MOOC « Apprendre à coder avec Python » concernant l’initialisation de tableaux, ni à utiliser Python Tutor et observer en particulier les diagrammes d’état.
+
+# Si rien ne marche : consultez la FAQ sur UpyLaB 5.23.
+
