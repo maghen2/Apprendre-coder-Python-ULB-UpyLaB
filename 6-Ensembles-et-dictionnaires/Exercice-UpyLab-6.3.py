19782281b8e71352b9ced6c1a6b90140144861fe
maghen2 <maghen2@gmail.com>
2024-06-07 05:53:39 +0200

    Exercice UpyLab 6.3

---
+++ 6-Ensembles-et-dictionnaires/Exercice-UpyLab-6.3.py
@@ -0,0 +1,17 @@
+# Exercice UpyLab 6.3 - Parcours : bleu rouge
+# Auteurs: Sébastien Hoarau, Thierry Massart, Isabelle Poirier, Jacky Trinh
+# Un jury doit attribuer le prix du « Codeur de l’année ».
+
+# Afin de récompenser les trois candidats ayant obtenu la meilleure moyenne, nous vous demandons d’écrire une fonction top_3_candidats(moyennes) qui reçoit un dictionnaire contenant comme clés les noms des candidats et comme valeurs la moyenne que chacun a obtenue.
+
+# Cette fonction doit retourner un tuple contenant les noms des trois meilleurs candidats, par ordre décroissant de leurs moyennes.
+
+# Exemple
+# L’appel suivant de la fonction :
+
+# top_3_candidats({'Candidat 7': 2, 'Candidat 2': 38, 'Candidat 6': 85,
+#                  'Candidat 1': 8, 'Candidat 3': 17, 'Candidat 5': 83,
+#                  'Candidat 4': 33})
+# doit retourner :
+
+# ('Candidat 6', 'Candidat 5', 'Candidat 2')
