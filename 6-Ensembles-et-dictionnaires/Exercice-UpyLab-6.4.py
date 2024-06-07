80606e89f85a86615a24bf6359f03ef885d58a27
maghen2 <maghen2@gmail.com>
2024-06-07 06:26:02 +0200

    Write a substitute(message, abbreviation) function that returns a copy of the message string in which the words that appear among the keys of the abbreviation dictionary are replaced by their meaning (value).

--- 6-Ensembles-et-dictionnaires/Exercice-UpyLab-6.4.py
+++ 6-Ensembles-et-dictionnaires/Exercice-UpyLab-6.4.py
@@ -18,3 +18,20 @@
 
 # 'Chuck Norris counted two times to infinity'
 
+def substitue(message, abreviation):
+    words = message.split()  # Split the message into individual words
+    result = []  # Create an empty list to store the substituted words
+    for word in words:
+        if word in abreviation:
+            result.append(abreviation[word])  # Replace the word with its corresponding value from the abbreviation dictionary
+        else:
+            result.append(word)  # Keep the word as it is if it is not found in the abbreviation dictionary
+    return ' '.join(result)  # Join the substituted words back into a single string with spaces in between
+
+
+resultat = substitue('C. N. cpt 2 to inf', {'C.' : 'Chuck',
+                                'N.' : 'Norris',
+                                'cpt' : 'counted',
+                                '2' : 'two times',
+                                'inf' : 'infinity'})
+print(resultat)  # Chuck Norris counted two times to infinity
