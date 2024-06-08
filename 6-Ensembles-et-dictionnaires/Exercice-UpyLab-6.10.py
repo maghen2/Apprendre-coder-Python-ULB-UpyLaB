9e42bf096e12b65e81fab77dbf53a4cc9f60f05b
maghen2 <maghen2@gmail.com>
2024-06-08 14:53:55 +0200

    Exercice UpyLab 6.10

---
+++ 6-Ensembles-et-dictionnaires/Exercice-UpyLab-6.10.py
@@ -0,0 +1,14 @@
+# Exercice UpyLab 6.10 - Parcours : rouge
+# Auteurs : Sébastien Hoarau - Thierry Massart - Isabelle Poirier
+# Écrire une fonction valeurs(dico) qui doit fournir, à partir du dictionnaire donné en paramètre, une liste des valeurs du dictionnaire triées selon leur clé.
+
+# Exemple
+# L’appel de la fonction suivant :
+
+# valeurs({'three': 'trois', 'two': 'deux', 'one': 'un'})
+# retourne :
+
+# ['un', 'trois', 'deux']
+# En effet, les clés correspondantes sont “one” < “three” < “two” (ordre UTF-8).
+
+ 
